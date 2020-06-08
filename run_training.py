f7c0eac99f70246d3ab18b3d1d6049cdc493466a
hanearlkim <hanearlkim226@gmail.com>
2020-06-08 19:54:51 +0900

    소스코드 정리

--- run_training.py
+++ run_training.py
@@ -23,6 +23,7 @@ if not os.path.isdir(config.tb_path):
 with open(os.path.join(config.data_path, "train_set.pkl"), "rb") as f:
     (train_x, train_y) = pickle.load(f)
 
+
 log_dir = os.path.join(config.tb_path, datetime.datetime.now().strftime("%Y%m%d-%H%M%s"))
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir)
 
@@ -30,7 +31,7 @@ adapter_size = None # use None to fine-tune all of BERT
 model = create_model(config.max_seq_len, adapter_size=adapter_size)
 
 model.fit(x=train_x, y=train_y,
-          validation_split=0.1,
+          validation_split=0.2,
           batch_size=config.batch_size,
           shuffle=True,
           epochs=config.num_epochs,
@@ -40,6 +41,5 @@ model.fit(x=train_x, y=train_y,
                                                     warmup_epoch_count=config.warmup_epoch_count,
                                                     total_epoch_count=config.num_epochs),
                      keras.callbacks.EarlyStopping(patience=20, restore_best_weights=True),
-
                      tensorboard_callback, MyCustomCallback()])
 model.save_weights(os.path.join(config.epoch_model_path, 'sentiments_fin.h5'), overwrite=True)
