588cb9796e5994d222bb1df203ecc7744d1dd620
hanearkim226@gmail.com <rlagksdjf1!>
2020-06-14 07:17:40 +0900

    loss 함수의 reduction 정의

--- create_model.py
+++ create_model.py
@@ -84,17 +84,28 @@ def create_model(max_seq_len, adapter_size=64):
         freeze_bert_layers(bert)
 
     def sigmoid_cross_entropy_loss(true, pred):
-
         loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=pred, labels=true)
         loss = tf.reduce_mean(tf.reduce_sum(loss))
         return loss
+
     focal_loss = BinaryFocalLoss(gamma=config.focal_gamma, from_logits=True)
+
+    def my_focal_loss(y_true, y_pred):
+        loss = focal_loss(y_pred=y_pred, y_true=y_true)
+        loss = tf.reduce_mean(tf.reduce_sum(loss))
+        return loss
+
     tfa_focal_loss = tfa.losses.SigmoidFocalCrossEntropy(alpha=config.focal_alpha, gamma=config.focal_gamma)
+
+    def my_tfa_focal_loss(y_true, y_pred):
+        loss = tfa_focal_loss(y_pred=y_pred, y_true=y_true)
+        loss = tf.reduce_mean(tf.reduce_sum(loss))
+        return loss
 
     loss_func_list = {
         "sigmoid_cross_entropy_loss": sigmoid_cross_entropy_loss,
-        "focal_loss": focal_loss,
-        "tfa_focal_loss": tfa_focal_loss
+        "focal_loss": my_focal_loss,
+        "tfa_focal_loss": my_tfa_focal_loss
     }
 
     model.compile(optimizer=keras.optimizers.Adam(),
