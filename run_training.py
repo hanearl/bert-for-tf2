5fbad8f9453248be1881f3b8d100d992de255701
hanearlkim <hanearlkim226@gmail.com>
2020-06-15 13:20:23 +0900

    train_y 범위 버그수정

--- run_training.py
+++ run_training.py
@@ -37,7 +37,7 @@ class ExamHelper:
         adapter_size = None # use None to fine-tune all of BERT
         model = create_model(self.config.max_seq_len, adapter_size=adapter_size)
 
-        model.fit(x=self.train_x[:self.config.train_len], y=self.train_y[self.config.train_len],
+        model.fit(x=self.train_x[:self.config.train_len], y=self.train_y[:self.config.train_len],
                   validation_split=0.2,
                   batch_size=self.config.batch_size,
                   shuffle=True,
